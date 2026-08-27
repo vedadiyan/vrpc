07353bd360f526679dd23e9dc4dc8372be82145a
Pouya Vedadiyan <vedadiyan@gmail.com>
2026-08-27 13:08:24 +0800

    added support for methods

--- handler.go
+++ handler.go
@@ -1,35 +1,47 @@
 package vrpc
 
 import (
+	"fmt"
 	"io"
 	"reflect"
+	"strings"
 
 	"github.com/vedadiyan/vapor"
+	"github.com/vedadiyan/vedio"
 )
 
-func CreateHandler[T any, R any](serviceName string, pattern vapor.Pattern) (vapor.Pattern, func(vapor.Request) vapor.Response) {
+func CreateHandler[T any, R any](serviceName string, method string, pattern vapor.Pattern) (vapor.Pattern, func(vapor.Request) vapor.Response) {
 	px, err := CreateProxy(serviceName, reflect.TypeFor[T](), reflect.TypeFor[R]())
 	if err != nil {
 		return "", nil
 	}
+	vedio.RegisterFor[func(vapor.Request) vapor.Response, func(vapor.Request) vapor.Response](vedio.WithName(fmt.Sprintf("%s.%s", serviceName, strings.ToLower(method))), vedio.WithGenerator(func() (func(vapor.Request) vapor.Response, error) {
+		return func(r vapor.Request) vapor.Response {
+			i, err := io.ReadAll(r.Content())
+			if err != nil {
+				return ToError(serviceName, err)
+			}
+			req, err := Decode[T](r.Headers().Get("Content-Type"), i)
+			if err != nil {
+				return ToError(serviceName, err)
+			}
+			res, err := px(req)
+			if err != nil {
+				return ToError(serviceName, err)
+			}
+			o, err := Encode(r.Headers().Get("Accept"), res)
+			if err != nil {
+				return ToError(serviceName, err)
+			}
+			return vapor.NewResponse(MapSuccessCode(serviceName), vapor.WithContent(o))
+		}, nil
+	}))
 	return pattern, func(r vapor.Request) vapor.Response {
-		i, err := io.ReadAll(r.Content())
+		fn, err := vedio.Resolve[func(vapor.Request) vapor.Response](vedio.WithName(fmt.Sprintf("%s.%s", serviceName, strings.ToLower(r.Method()))))
 		if err != nil {
 			return ToError(serviceName, err)
 		}
-		req, err := Decode[T](r.Headers().Get("Content-Type"), i)
-		if err != nil {
-			return ToError(serviceName, err)
-		}
-		res, err := px(req)
-		if err != nil {
-			return ToError(serviceName, err)
-		}
-		o, err := Encode(r.Headers().Get("Accept"), res)
-		if err != nil {
-			return ToError(serviceName, err)
-		}
-		return vapor.NewResponse(MapSuccessCode(serviceName), vapor.WithContent(o))
+		return fn(r)
 	}
 }
 
